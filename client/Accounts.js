a07555d4da80c1b98db761ec311c30a902858c86
Andre Sirikaku <asirikaku2000@gmail.com>
2018-07-29 19:16:41 -0300

    implemnetar o login

--- client/Accounts.js
+++ client/Accounts.js
@@ -7,3 +7,33 @@ AccountsTemplates.configure({
 	defaultContentRegion: 'main'
 });
 AccountsTemplates.configureRoute('signIn');
+
+T9n.setLanguage("pt");
+
+var pwd = AccountsTemplates.removeField("password");
+AccountsTemplates.removeField("email");
+AccountsTemplates.addFields([
+  {
+    _id: "username",
+    type: "text",
+    displayName: "username",
+    required: true,
+    minLength: 3,
+  },
+  {
+    _id: "email",
+    type: "email",
+    required: true,
+    displayName: "email",
+    re: /.+@(.+){2,}\.(.+){2,}/,
+    errStr: "Invalid email",
+  },
+  pwd,
+  {
+    _id: "name",
+    type: "text",
+    displayName: "Nome Completo",
+    required: true
+  },
+]);
+

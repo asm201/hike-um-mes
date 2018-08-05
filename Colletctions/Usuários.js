7610310c988c1bdc86fb9ca08b59261c3acdc6d7
Andre Sirikaku <asirikaku2000@gmail.com>
2018-08-05 15:02:57 -0300

    Adicionar botão de seguir e deixar de seguir.

---
+++ Colletctions/Usuários.js
@@ -0,0 +1,28 @@
+Meteor.methods({
+    "seguirUsuario": function(idDoUsuario) {
+        Meteor.users.update(Meteor.userId(), {
+            $addToSet: {
+                "profile.seguindo": idDoUsuario
+            }
+        });
+
+        Meteor.users.update(idDoUsuario, {
+            $addToSet: {
+                "profile.seguidores": Meteor.userId()
+            }
+        });
+    },
+    "deixarDeSeguirUsuario": function(idDoUsuario) {
+        Meteor.users.update(Meteor.userId(), {
+            $pull: {
+                "profile.seguindo": idDoUsuario
+            }
+        });
+
+        Meteor.users.update(idDoUsuario, {
+            $pull: {
+                "profile.seguidores": Meteor.userId()
+            }
+        });
+    }
+});
